41844912ec04e7945827d0ddd6eebcf7699ab81d
Anastasiya Chernak <anastasiya.chernak@okta.com>
2023-10-18 11:04:28 +0200

    add custom content

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,26 @@ import math
 import pandas as pd
 import streamlit as st
 
+
+st.set_page_config(
+    page_title="Chatbot",
+    page_icon='💬',
+    layout='wide'
+)
+
+st.header("Chatbot Implementation")
+st.write("""
+LangChain is a framework for developing applications powered by language models. It enables applications that:
+
+Are context-aware: connect a language model to sources of context (prompt instructions, few shot examples, content to ground its response in, etc.)
+Reason: rely on a language model to reason (about how to answer based on provided context, what actions to take, etc.)
+The main value props of LangChain are:
+
+Components: abstractions for working with language models, along with a collection of implementations for each abstraction. Components are modular and easy-to-use, whether you are using the rest of the LangChain framework or not
+Off-the-shelf chains: a structured assembly of components for accomplishing specific higher-level tasks
+Off-the-shelf chains make it easy to get started. For complex applications, components make it easy to customize existing chains and build new ones.
+         """)
+
 """
 # Welcome to Streamlit!
 
